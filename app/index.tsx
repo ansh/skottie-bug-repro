938ff687407d7869378cfc2808b2df81ab512e66
Ansh Nanda <anshnanda10@gmail.com>
2024-05-01 20:04:38 -0700

    create bug repro

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,61 @@
-import { Stack, Link } from 'expo-router';
+import { useCallback, useState, useEffect } from 'react';
+import { View, Text, Dimensions } from 'react-native';
+import { Skottie } from 'react-native-skottie';
 
-import { Button } from '~/components/Button';
-import { Container } from '~/components/Container';
-import { ScreenContent } from '~/components/ScreenContent';
+const now = new Date(); // Current date
+const currentWeekStartDate = new Date(now.setDate(now.getDate() - now.getDay() + 1)); // Monday
+const currentWeekEndDate = new Date(now.setDate(now.getDate() - now.getDay() + 7)); // Sunday
+const screenWidth = Dimensions.get('window').width;
+const dayIndexToDay = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
-export default function Home() {
+const NUMBER_OF_LOTTIES_TO_RENDER = 1; // no crash
+// const NUMBER_OF_LOTTIES_TO_RENDER = 7; // crash
+
+const CalendarViewCustom = () => {
+  const renderDaySquare = useCallback((dayIndex: number) => {
+    const shouldLottieRender = false; // turn this to true to see the animation
+
+    return (
+      <View className="flex-col items-center" key={dayIndex}>
+        <Text className="text-gray-800">{dayIndexToDay[dayIndex]}</Text>
+        <View
+          key={dayIndex}
+          className={`${shouldLottieRender ? 'bg-blue-500' : 'bg-gray-200'} m-1 rounded-md`}
+          style={{
+            width: (screenWidth - 84) / 7,
+            height: (screenWidth - 96) / 7,
+          }}>
+          {shouldLottieRender ? (
+            <Skottie
+              source={require('../assets/flame.json')}
+              autoPlay
+              loop
+              speed={3}
+              style={{
+                width: (screenWidth - 96) / 7,
+                height: (screenWidth - 96) / 7,
+              }}
+            />
+          ) : null}
+        </View>
+      </View>
+    );
+  }, []);
+
   return (
-    <>
-      <Stack.Screen options={{ title: 'Home' }} />
-      <Container>
-        <ScreenContent path="app/index.tsx" title="Home" />
-        <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
-          <Button title="Show Details" />
-        </Link>
-      </Container>
-    </>
+    <View className="justify-between gap-y-1">
+      <Text className="text-xl">
+        {currentWeekStartDate.toLocaleString('default', { month: 'short' })}{' '}
+        {currentWeekStartDate.getDate()}-{currentWeekEndDate.getDate()}
+      </Text>
+
+      <View className="flex-row">
+        {Array.from({ length: NUMBER_OF_LOTTIES_TO_RENDER }).map((_, index) =>
+          renderDaySquare(index)
+        )}
+      </View>
+    </View>
   );
-}
+};
+
+export default CalendarViewCustom;
